c843964e5f38a48965e1134897195d259b9c6b07
LC-Cthulhu <andrew.easterday@live.com>
2018-10-02 11:35:47 -0400

    Add project files.

---
+++ QPSTheGame/QPSTheGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QPSTheGame
+{
+    public partial class QPSTheGame : Form
+    {
+        public QPSTheGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
