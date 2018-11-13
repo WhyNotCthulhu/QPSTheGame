1c594a8b5eb5e03a94df1e68b4dc36f0fb23a002
WhyNotCthulhu <andrew.easterday@live.com>
2018-11-13 03:08:57 -0500

    Initial Commit
    Added classes and main GUI.

--- QPSTheGame/QPSTheGame.cs
+++ QPSTheGame/QPSTheGame.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Engine;
 
 namespace QPSTheGame
 {
     public partial class QPSTheGame : Form
     {
+        private Player _player;
+
         public QPSTheGame()
         {
             InitializeComponent();
+
+            Location location = new Location(1, "Home", "This is your house.");
+            location.ID = 1;
+            location.Name = "Home";
+            location.Description = "This is your house.";
+
+            _player = new Player(10, 10, 20, 0, 1);
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void QPSTheGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
